b2f712eabed7a8a2c156c703283b718a5e286e38
ISROHarsh <120256045+ISROHarsh@users.noreply.github.com>
2022-12-12 16:04:46 +0530

    🍀Initial commit

---
+++ examples.py
@@ -0,0 +1,23 @@
+from premsir import *
+
+'''
+Inputs a string and performs following operation
+    a) Number of small alphabets in it
+    b) Number of capital alphabets in it
+    c) Number of digits in it
+    d) Length of the string
+    e) Make first letter of each word capital 
+'''
+# s = input("Enter a string> ")
+# premsir.string_operations(s)
+
+# Should display the fibonacci series for n terms
+# t = int(input("Enter the number of terms> "))
+# premsir.fibonacci_series(t)
+
+# Find the hcf of two integers
+# a = int(input("Enter first number> "))
+# b = int(input("Enter second number> "))
+# premsir.gcd(a, b)
+
+premsir.greatest_integer(2, 3, 5)
---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='premsir',
+    version='0.0.1',    
+    description='Programming in a nutshell for grade 11.',
+    license='MIT License',
+    author='Prem Kumar Singh',
+    packages=['premsir'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ]
+)
